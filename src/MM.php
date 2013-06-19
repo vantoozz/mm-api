f63571737f78dbf7c6cb5c8435ad6ad22e088cbb
Ivan Nikitin <vantoozz@gmail.com>
2013-06-19 14:31:03 +0400

    + api method

--- src/MM.php
+++ src/MM.php
@@ -14,5 +14,34 @@ class MM{
 	
 	public function app_id(){
 		return $this->app_id;
-	}	
+	}
+
+    public function api($method, $params){
+        $params['app_id'] = $this->app_id;
+        $params['method'] = $method;
+        $params['format'] = 'json';
+        $params['secure'] = 1;
+        $params['sig']=$this->sign($params);
+
+        $response=file_get_contents('http://www.appsmail.ru/platform/api?'.http_build_query($params));
+        if(!$response=json_decode($response)){
+            throw new MMException('MM API error');
+        }
+        return $response;
+    }
+
+
+
+    public function sign($params){
+        $sign='';
+        ksort($params);
+        foreach($params as $key=>$value){
+            if('sig' == $key){
+                continue;
+            }
+            $sign.=$key.'='.$value;
+        }
+        $sign.=$this->secret_key;
+        return md5($sign);
+    }
 }
